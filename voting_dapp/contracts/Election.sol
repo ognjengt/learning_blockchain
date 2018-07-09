9cec46b24b51820f2f1e1a009f48e6d2fd8c8f85
Ognjen Gatalo <ognjeng95@gmail.com>
2018-07-09 13:00:17 +0200

    Completed app.js and implemented new contract

--- voting_dapp/contracts/Election.sol
+++ voting_dapp/contracts/Election.sol
@@ -2,10 +2,27 @@ pragma solidity ^0.4.11;
 
 contract Election {
 
-  string public candidate;
+  // Model a Candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
 
+  // Store Candidates
+  mapping(uint => Candidate) public candidates;
+  // Fetch Candidate
+  // Store Candidates Count
+  uint public candidatesCount;
+  
   function Election() public {
-    candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate(string _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
   
 }
